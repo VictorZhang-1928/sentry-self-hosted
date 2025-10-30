de0c9ddf835e9cfcebfc9c16460467ff65ec8e9c
VictorZhang-1928 <vhackerv2002@gmail.com>
2025-10-30 10:15:12 +0800

    环境变量调整

--- install/ensure-relay-credentials.sh
+++ install/ensure-relay-credentials.sh
@@ -6,7 +6,8 @@ RELAY_CREDENTIALS_JSON=relay/credentials.json
 ensure_file_from_example $RELAY_CONFIG_YML
 
 # Replace ENV vars from .env into relay/config.yml
-envsubst <"$RELAY_CONFIG_YML" >"$RELAY_CONFIG_YML".tmp
+source .env
+envsubst '$REDIS_PASSWORD' < "$RELAY_CONFIG_YML" >"$RELAY_CONFIG_YML".tmp
 mv "$RELAY_CONFIG_YML".tmp "$RELAY_CONFIG_YML"
 
 if [[ -f "$RELAY_CREDENTIALS_JSON" ]]; then
